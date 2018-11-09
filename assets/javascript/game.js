78b69537bb8d6fc4b646142bb834d0c62319af43
Ross Gongaware <rossgong@gmail.com>
2018-11-09 00:29:15 -0500

    Changed some of the internal setup to allow for a possible full reset down the road. Fixed some balance issues in character stats. Still need TODO something when the player wins or loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,17 @@
+
+/*
+	Laid out like this
+	["Character Name", baseAttackStat, counterStat, hp, "path/to/characterImage.png"]
+	To add more characters, just add info to this list.
+*/
 var characterStatList = [
-	["Beat", 6, 6, 100, "assets/images/beat.png"],
-	["Goji", 4, 8, 200, "assets/images/goji.png"],
-	["Jazz", 7, 6, 80, "assets/images/jazz.png"],
-	["Poison Jam", 5, 9, 75, "assets/images/poison.png"]
+	["Beat", 6, 30, 100, "assets/images/beat.png"],
+	["Goji", 4, 10, 200, "assets/images/goji.png"],
+	["Jazz", 7, 35, 80, "assets/images/jazz.png"],
+	["Poison Jam", 5, 10, 75, "assets/images/poison.png"]
 ]
+
+// function createReset
 
 function Character(name, attack, counter, hp, image, index) {
 
@@ -28,8 +36,9 @@ function Character(name, attack, counter, hp, image, index) {
 
 		//Label div contains the name and the HP
 		var label = $("<div>", { class: "char-label" });
-		this.hpLabel = $("<p>", { class: "char-label-hp" }).text(this.hp)
 		label.append($("<p>", { class: "char-label-name" }).text(this.name));
+
+		this.hpLabel = $("<p>", { class: "char-label-hp" }).text(this.hp)
 		label.append(this.hpLabel);
 		return div.append(label);
 	}
@@ -45,23 +54,27 @@ function Character(name, attack, counter, hp, image, index) {
 	};
 
 	this.updateHPHTML = function () {
-		this.hpLabel.html(this.hp);
+		this.hpLabel.text(this.hp);
 		if (this.hp < 50) {
 			this.hpLabel.css("color", "red");
+			if (this.hp < 0) {
+				this.hpLabel.text("0");
+			}
 		}
-		console.log(this);
 	};
 
 	this.attack = function (defender) {
-		defender.hp -= this.attackStat;
-		this.attackStat += this.baseAttack;
+		if (this.isAlive()) {
+			defender.hp -= this.attackStat;
+			this.attackStat += this.baseAttack;
 
-		if (defender.isAlive()) {
-			this.hp -= defender.counterStat;
+			if (defender.isAlive()) {
+				this.hp -= defender.counterStat;
+			}
+
+			defender.updateHPHTML();
+			this.updateHPHTML();
 		}
-
-		defender.updateHPHTML();
-		this.updateHPHTML();
 	};
 
 	this.isAlive = function () {
@@ -95,19 +108,28 @@ var game = {
 		$("#player-blank").remove();
 		this.playerCharacter.card.appendTo("#player-char");
 
+		$("#char-select-label").text("Select your enemy")
+		$("#enemy-blank").append($("<div>", { class: "char-label" }).append($("<p>", { class: "char-label-name" }).text("SELECT YOUR ENEMY")));
 	},
 
 	setDefender: function (index) {
 		this.currentDefender = this.removeCharacter(index);
 
-		$("#defender-blank").remove();
+		if (this.characterList.length == 0) {
+			var blank = $("#enemy-blank");
+			blank.css("opacity", 0);
+			$("#enemy-blank").appendTo("#char-select");
+		} else {
+			$("#enemy-blank").remove();
+		}
+
 		this.currentDefender.card.appendTo("#defender-char");
 	},
 
 	removeDefender: function () {
 		this.currentDefender.card.remove();
 		this.currentDefender = {};
-		this.createDefenderBlank().appendTo("#defender-char");
+		this.createBlank(false).appendTo("#defender-char");
 	},
 
 	removeCharacter: function (index) {
@@ -124,26 +146,47 @@ var game = {
 		for (var i = 0; i < this.characterList.length; i++) {
 			$("#char-select").append(this.characterList[i].card);
 		}
+		$("#player-char").append(this.createBlank(true));
+		var blank = this.createBlank(false);
+		blank.find("div").remove();
+
+		$("#defender-char").append(blank);
 	},
 
-	createDefenderBlank: function () {
+	createBlank: function (isPlayer) {
 		//Containing div for the image and the label		
-		var div = $("<div>", { class: "char-card enemy-card", id: "defender-blank" });
+		var cardClass = "char-card ";
+		var cardID = "";
+		if (isPlayer) {
+			cardClass += "player-card";
+			cardID = "player-blank"
+		} else {
+			cardClass += "enemy-card";
+			cardID = "enemy-blank"
+		}
+		var div = $("<div>", { class: cardClass, id: cardID, value: -1 });
 		div.append($("<img>", { class: "char-img", src: "assets/images/blank.png", alt: "blank" }));
+
+		div.append($("<div>", { class: "char-label" }).append($("<p>", { class: "char-label-name" }).text("SELECT A CHARACTER!")));
+		console.log(div);
 		return div;
 	},
 
 	intializeGame: function (characterStats) {
+		this.characterList = [];
+		this.playerCharacter = {};
+		this.currentDefender = {};
+
 		for (var i = 0; i < characterStats.length; i++) {
 			this.characterList.push(new Character(characterStats[i][0], characterStats[i][1], characterStats[i][2], characterStats[i][3], characterStats[i][4], i))
 		};
 
-		this.createDefenderBlank().appendTo("#defender-char");
-
 		this.initializeHTML();
 	},
 
+	lose: function () {
 
+	}
 }
 
 $(document).ready(function () {
@@ -151,16 +194,20 @@ $(document).ready(function () {
 
 	$(".char-card").on("click", function (event) {
 		var index = this.getAttribute("value");
-		var char = game.characterList[index];
 
-		if (game.hasChosenCharacter()) {
-			if (char !== game.playerCharacter) {
-				if (!game.hasDefender()) {
-					game.setDefender(index);
+		//blanks have a value of -1 to prevent anything happening when they are clicked.
+		if (index != -1) {
+			var char = game.characterList[index];
+
+			if (game.hasChosenCharacter()) {
+				if (char !== game.playerCharacter) {
+					if (!game.hasDefender()) {
+						game.setDefender(index);
+					}
 				}
+			} else {
+				game.setChoosenCharacter(index);
 			}
-		} else {
-			game.setChoosenCharacter(index);
 		}
 	});
 
@@ -168,13 +215,13 @@ $(document).ready(function () {
 		if (game.hasChosenCharacter() && game.hasDefender()) {
 			game.playerCharacter.attack(game.currentDefender);
 
+			if (!game.playerCharacter.isAlive()) {
+				game.lose();
+			}
+
 			if (!game.currentDefender.isAlive()) {
 				game.removeDefender();
 			}
 		}
 	});
-	console.log(game.characterList);
-
-
-
 });
